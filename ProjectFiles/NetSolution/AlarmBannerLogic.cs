d00accc8329b0c3cfade7647f58c7b34c9305b4e
Sbodhak1 <subhajit.bodhak@rockwellautomation.com>
2022-08-18 15:15:38 +0530

    UA Client Data Updated

---
+++ ProjectFiles/NetSolution/AlarmBannerLogic.cs
@@ -0,0 +1,31 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.OPCUAClient;
+using FTOptix.WebUI;
+using FTOptix.OPCUAServer;
+using FTOptix.NetLogic;
+using FTOptix.CoreBase;
+using FTOptix.EventLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Alarm;
+using FTOptix.Core;
+#endregion
+
+public class AlarmBannerLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
